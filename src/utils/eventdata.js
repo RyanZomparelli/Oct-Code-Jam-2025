8b0c0643493adbb2f770b53aa64fcaffc935d30a
Ryan Zomparelli <zomp.ryan@gmail.com>
2025-10-13 07:01:50 -0400

    Add id property to each event in eventdata.js

--- src/utils/eventdata.js
+++ src/utils/eventdata.js
@@ -1,5 +1,6 @@
 export const events = [
   {
+    id: 0,
     image:
       "https://images.unsplash.com/photo-1508361001413-7a9dca21d08a?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8aGFsbG93ZWVufGVufDB8fDB8fHwy&auto=format&fit=crop&q=60&w=900",
     title: "Pumpkin Carving Madness",
@@ -7,6 +8,7 @@ export const events = [
     description: "Come for a pumpkin carving contest and win prizes",
   },
   {
+    id: 1,
     image:
       "https://images.unsplash.com/photo-1476283721796-dd935b062838?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGhhbGxvd2VlbnxlbnwwfHwwfHx8Mg%3D%3D&auto=format&fit=crop&q=60&w=900",
     title: "Pumpkin Spice Fall Fest",
@@ -14,6 +16,7 @@ export const events = [
     description: "SPECIAL: Half off your favorite fall drink",
   },
   {
+    id: 2,
     image:
       "https://images.unsplash.com/photo-1509557965875-b88c97052f0e?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGFsbG93ZWVufGVufDB8fDB8fHwy&auto=format&fit=crop&q=60&w=900",
     title: "Halloween Decoration Neighborhood Tour",
@@ -22,6 +25,7 @@ export const events = [
       "Join us for a walk around the best decorated neighborhoods in the city",
   },
   {
+    id: 3,
     image:
       "https://images.unsplash.com/photo-1532767153582-b1a0e5145009?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGhhbGxvd2VlbnxlbnwwfHwwfHx8Mg%3D%3D&auto=format&fit=crop&q=60&w=900",
     title: "Full Moon Seance",
